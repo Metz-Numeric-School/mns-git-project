d07e34649f81bf8b7ecc1a2b763bd74d08982331
GamorySensei <theo.gamory@metznumericschool.fr>
2024-04-04 14:33:56 +0200

    [NEW] > Implémentation du système de routage des pages

--- public/index.php
+++ public/index.php
@@ -1,2 +1,22 @@
 <?php
 
+if(!empty($_GET['page']))
+{
+    $page = $_GET['page'];
+}
+else
+{
+    $page = "index";
+}
+
+$filepath = "../src/pages/" . $page . ".php";
+if(file_exists($filepath))
+{
+    require $filepath;
+}
+else
+{
+    header("HTTP/1.1 404 Not Found");
+    echo "Page not found";
+    die;
+}
